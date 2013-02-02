2365e8b7031769ceb4bce27e2410f6c4570e2843
jhristov <jrdnhr@gmail.com>
2013-02-02 04:29:30 -0800

    Sprites.java
    Basic skeleton for the sprites

---
+++ Sprite.java
@@ -0,0 +1,57 @@
+import java.awt.Image;
+
+public class Sprite {
+  
+	int x;
+	int y;
+	int power;
+	double angle;
+	boolean visible;
+	boolean dead;
+	Image image;
+	
+	public int GetX(){
+		return this.x;
+	}
+	
+	public void SetX(int x){
+		this.x = x;
+	}
+	
+	public int GetY(){
+		return this.y;
+	}
+	
+	public void SetY(int y){
+		this.y = y;
+	}
+	
+	public boolean GetDead(){
+		return this.dead;
+	}
+	
+	public void SetDead(boolean dead){
+		this.dead = dead;
+	}
+	
+	public void SetVisible(boolean visible){
+		this.visible = visible;
+	}
+	
+	public boolean IsVisible(){
+		return this.visible;
+	}
+	
+	public void Die(){
+		this.visible = false;
+	}
+	
+	public void SetImage(Image image){
+		this.image = image;
+	}
+	
+	public Image GetImage(){
+		return this.image;
+	}
+
+}
